e95f95e524c4c15553fdf4e52b109461d3c2297a
Mohib Abdali <mohib6568@gmail.com>
2025-07-30 19:15:08 +0500

    image paths updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import 'firstpage.dart'; // import your first page here
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Clothing App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const FirstPage(),
+    );
+  }
+}
